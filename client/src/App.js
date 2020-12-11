4ce398dbfb135a1ecf46fd9024430afe0a5cfdcd
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-11 20:10:16 +0700

    Experiment with mysql data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState([]);
+  const [test , setTest] = useState(null);
+
+  const getEmployees = () => {
+    console.log("clicked");
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      const newQuery = response.data;
+      setQuery(newQuery);
+    })
+
+    updateResult();
+  }
+
+  let result = test;
+
+  const updateResult = () => {
+    console.log("update result called");
+    setTest(<div> Test</div>);
+  }
+
   return (
     <div className="App"> 
-    Hello world
+      {result}
+      <h1>Hello world</h1>
+      <button onClick={getEmployees}>Get Employee</button>
+      <button onClick={updateResult}>Show Employees</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const expressApp = express(); 
 const mysql = require('mysql');
+const cors = require('cors');
+
+expressApp.use(cors());
 
 const currentDB = mysql.createConnection({
     user: 'root',
     host: 'localhost',
     password: '',
-    database = 'w12',
-}
-)
+    database: 'w12',
+});
 
+expressApp.get('/employees', (req, res) => { 
+    currentDB.query("SELECT * FROM anggota", (err, result) => {
+        if(err) {
+            console.log("error");
+        }
+        else{
+            res.send(result);
+        }
+    }); 
+})
+ 
 expressApp.listen(3001, () => {
     console.log("Server is running on port 3001");
 })
