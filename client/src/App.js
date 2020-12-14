afe52bb58e6763d078396363a746d6082b2c2f76
Rainaldi Satria <57592497+rainaldisatria@users.noreply.github.com>
2020-12-14 18:24:15 +0700

    Finish setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,22 @@
 import './App.css';
 import Axios from 'axios';
-import React, { useState } from "react";
-import {connect} from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import * as actions from './store/actions';
 
-function App() {
-  const [queryResult, setQueryResult] = useState([]); // Object that contain all of query result (SELECT * FROM anggota).
-  const [columns, setColumns] = useState([]);  // Column name. 
-
-  //#region 
-  const [addFields, setAddFields] = useState({});
-  //#endregion
-
-  //#region API
-  const getQueryResult = () => {
-    Axios.get("http://localhost:3001/getTable",).then((response) => {
-      setQueryResult(response.data);
-
-      let newColumns = [];
-      Object.keys(response.data[0]).map((value, index) => {
-        return newColumns.push(value);
-      })
-      setColumns(newColumns);
-    }).catch((error) => {
-      console.log(error);
-    })
-  }
-
-  const deleteTable = (columnName, value) => {
-    Axios.post("http://localhost:3001/delete",
-      {
-        columnName: columnName,
-        value: value
-      }
-    ).then((response) => {
-      getQueryResult();
-    })
-
-  }
-
-  const insertQueryResult = (e) => {
-    e.preventDefault();
-
-    Axios.post("http://localhost:3001/insert", addFields).then((response) => {
-      getQueryResult();
-    })
-  }
-  //#endregion 
+function App(props) {
+  const [addFields, setAddFields] = useState({}); 
 
   return (
     <div className="App">
       <h1>Apotek Jakarta</h1>
-      <button onClick={getQueryResult}>Intialize</button>
+      <button onClick={this.props.fetchSelectedTable}>Intialize</button>
 
       <table style={{ width: "100%" }}>
         <thead>
           <tr>
             { // Render table column name 
-              columns.map((columnName, index) => {
+              this.props.columnsName.map((columnName, index) => {
                 return <th key={index}>{columnName}</th>
               })}
             <th>Action</th>
@@ -64,15 +24,15 @@ function App() {
         </thead>
         <tbody>
           {// Render table content  
-            queryResult.map((val, key) => {
+            this.props.tableData.map((val, key) => {
               return (
                 <tr key={key}>
                   {Object.keys(val).map((keyName, index) => {
                     return (<td key={key + " " + index}>{val[keyName]}</td>)
                   })}
                   <td>
-                    <button onClick={() => deleteTable(columns[0], val[Object.keys(val)[0]])}>Edit</button>
-                    <button onClick={() => deleteTable(columns[0], val[Object.keys(val)[0]])}>Delete</button>
+                    <button onClick={() => this.props.deleteTable(this.props.columnsName[0], val[Object.keys(val)[0]])}>Edit</button>
+                    <button onClick={() => this.props.deleteTable(this.props.columnsName[0], val[Object.keys(val)[0]])}>Delete</button>
                   </td>
                 </tr>
               )
@@ -84,7 +44,7 @@ function App() {
       <h3>Add Anggota: </h3>
       {
         <form>
-          {columns.map((value, index) => {
+          {this.props.columnsName.map((value, index) => {
             return (
               <input
                 key={index}
@@ -94,26 +54,36 @@ function App() {
                   setAddFields((prevValue) => {
                     return {
                       ...prevValue,
-                      [columns[index]]: value,
+                      [this.props.columnsName[index]]: value,
                     }
                   })
                 }}
               ></input>
             )
           })}
-          <button onClick={(e) => insertQueryResult(e)}>Add</button>
+          <button onClick={(e) => {
+            e.preventDefault();
+            this.props.insertTotable(addFields);
+          }}>Add</button>
         </form>
       }
     </div>
   );
 }
 
-const mapStateToPropos = (stateFromRedux) => {
+const mapStateToPropos = (stateFromStore) => {
   return {
-    table: stateFromRedux.table,
-    tableData: stateFromRedux.tableData,
-    columnsName: stateFromRedux.columnsName,
+    tableData: stateFromStore.tableData,
+    columnsName: stateFromStore.columnsName,
   }
-} 
+}
 
-export default connect(mapStateToPropos)(App);
+const mapToDispatch = (dispatch) => {
+  return {
+    deleteTable: (columnName, value) => dispatch(actions.deleteFromSelectedTable(columnName, value)),
+    insertToTable: (objectToAdd) => dispatch(actions.insertToSelectedTable(objectToAdd)),
+    fetchSelectedTable: () => dispatch(actions.fetchSelectedTable()),
+  }
+}
+
+export default connect(mapStateToPropos, mapToDispatch)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './store/reducer';
@@ -11,10 +11,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 ); 
