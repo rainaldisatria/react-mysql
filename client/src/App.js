e07ddacdcf409c4f43f3cdf2496b0f45d19a225d
Rainaldi Satria <57592497+rainaldisatria@users.noreply.github.com>
2020-12-16 17:10:28 +0700

    Add material-UI to project

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import React from "react";
 import EditableTable from './components/EditableTable/EditableTable';
 import EditMenu from './components/EditMenu/EditMenu';
-import sendNotification, {Notification} from './components/Notification/Notification';
+import {Notification} from './components/Notification/Notification';
 
 function App() {
   return (
     <div className="App">
       {/*HOC*/}
       <Notification />
-      <EditMenu></EditMenu> 
-      <button onClick={() => sendNotification("Inserted", "success", 1)}>Send notification</button>
+      <EditMenu></EditMenu>  
       {/*Component*/}
       <h1>Apotek Jakarta</h1>
       <br></br>
