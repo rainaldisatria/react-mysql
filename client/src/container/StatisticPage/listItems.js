1439bc441d8d2443c09e969e1f9ad793fe08d787
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-24 21:08:56 +0700

    Update listItems.js

--- client/src/container/StatisticPage/listItems.js
+++ client/src/container/StatisticPage/listItems.js
@@ -9,69 +9,88 @@ import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import { Link } from 'react-router-dom';
 
 export const produk = (
   <div>
     <ListSubheader inset>Produk</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="List Obat" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Persediaan" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Log Perubahan" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Obat Kadaluarsa" />
-    </ListItem> 
-  </div>
+    <Link to='/admin/listObat'>
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="List Obat" />
+      </ListItem>
+    </Link>
+    <Link to='/admin/persediaan'>
+      <ListItem button>
+        <ListItemIcon>
+          <ShoppingCartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Persediaan" />
+      </ListItem>
+    </Link>
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Log Perubahan" />
+      </ListItem>
+    </Link>
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Obat Kadaluarsa" />
+      </ListItem>
+    </Link>
+  </div >
 );
 
 export const statistik = (
   <div>
     <ListSubheader inset>Statistik</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Ringkasan Penjualan" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Pesanan" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Transaksi" />
-    </ListItem>
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Ringkasan Penjualan" />
+      </ListItem>
+    </Link>
+
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Pesanan" />
+      </ListItem>
+    </Link>
+
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Transaksi" />
+      </ListItem>
+    </Link>
   </div>
 );
 
 export const superAdmin = (
   <div>
     <ListSubheader inset>Super Admin</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Control Panel" />
-    </ListItem> 
+    <Link>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Control Panel" />
+      </ListItem>
+    </Link>
   </div>
 );
