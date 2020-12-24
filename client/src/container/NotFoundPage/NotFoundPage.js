0f195b75ce0d0dd2668715758c456739b7c9c5fe
Rainaldi Satria <57592497+rainaldisatria@users.noreply.github.com>
2020-12-24 15:10:46 +0700

    NotFoundPage on place

--- client/src/container/NotFoundPage/NotFoundPage.js
+++ client/src/container/NotFoundPage/NotFoundPage.js
@@ -13,6 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import { useHistory } from 'react-router';
 
 function Copyright() {
     return (
@@ -37,32 +38,12 @@ const useStyles = makeStyles((theme) => ({
     },
     heroButtons: {
         marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-        paddingTop: theme.spacing(8),
-        paddingBottom: theme.spacing(8),
-    },
-    card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flexGrow: 1,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
-    },
-}));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }, 
+})); 
 
 const NotFoundPage = () => {
     const classes = useStyles();
+    const history = useHistory();
 
     return (
         <React.Fragment>
@@ -73,22 +54,18 @@ const NotFoundPage = () => {
                     <Container maxWidth="sm">
                         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
                             Waduh, tujuanmu nggak ada!
-            </Typography>
+                        </Typography>
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Mungkin kamu salah jalan atau alamat. Ayo balik sebelum gelap!
-            </Typography>
+                        </Typography>
                         <div className={classes.heroButtons}>
-                            <Grid container spacing={2} justify="center">
-                                <Grid item>
-                                    <Button variant="contained" color="primary">
-                                        Main call to action
-                  </Button>
-                                </Grid>
-                                <Grid item>
-                                    <Button variant="outlined" color="primary">
-                                        Secondary action
-                  </Button>
-                                </Grid>
+                            <Grid container spacing={2} justify="center"> 
+                                    <Button variant="contained" color="primary"
+                                    onClick={() => {
+                                        history.push('/');
+                                    }}>
+                                        Kembali
+                                    </Button> 
                             </Grid>
                         </div>
                     </Container>
