80d5e744a8ba74e46ba41dd935fef76bf5e3c976
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-25 16:57:47 +0700

    Showing 3 table-view (Januari, Februari, Maret)

--- client/src/container/AdminPage/Statistic/StatisticPage.js
+++ client/src/container/AdminPage/Statistic/StatisticPage.js
@@ -4,6 +4,7 @@ import Deposits from '../Deposits';
 import Orders from '../Orders';
 import clsx from 'clsx';
 import Chart from '../Chart';
+import EditableTable from '../../../components/EditableTable/EditableTable'
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
@@ -31,49 +32,63 @@ const StatisticPage = () => {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return <main className={classes.content}>
+  return <Container maxWidth="lg" className={classes.container}>
     <div className={classes.appBarSpacer} />
-    <Container maxWidth="lg" className={classes.container}>
-      <Typography component="h1" variant="h2" align="left" color="textPrimary" gutterBottom>
-        Ringkasan Penjualan
+
+    <Typography component="h1" variant="h2" align="left" color="textPrimary" gutterBottom>
+      Ringkasan Penjualan
       </Typography>
 
-      <Grid container spacing={3}>
-        {/* Chart */}
-        <Grid item xs={12} md={8} lg={9}>
-          <Paper className={fixedHeightPaper}>
-            <Chart />
-          </Paper>
-        </Grid>
-
-        {/* Recent Deposits */}
-        <Grid item xs={12} md={4} lg={3}>
-          <Paper className={fixedHeightPaper}>
-            <Deposits />
-          </Paper>
-        </Grid>
-        
-        {/* Recent Orders */}
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>
-            <Orders />
-          </Paper>
-        </Grid>
-
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>
-            <Orders />
-          </Paper>
-        </Grid>
-
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>
-            <Orders />
-          </Paper>
-        </Grid>
+    <Grid container spacing={3}>
+      {/* Chart */}
+      <Grid item xs={12} md={8} lg={9}>
+        <Paper className={fixedHeightPaper}>
+          <Chart />
+        </Paper>
       </Grid>
-    </Container>
-  </main>
+
+      {/* Recent Deposits */}
+      <Grid item xs={12} md={4} lg={3}>
+        <Paper className={fixedHeightPaper}>
+          <Deposits />
+        </Paper>
+      </Grid>
+
+      {/* Recent Orders */}
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="left" color="textPrimary" gutterBottom>
+            Januari
+          </Typography>
+          <EditableTable
+            tableName='view_januari'
+          />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="left" color="textPrimary" gutterBottom>
+            Februari
+          </Typography>
+          <EditableTable
+            tableName='view_februari'
+          />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="left" color="textPrimary" gutterBottom>
+            Maret
+          </Typography>
+          <EditableTable
+            tableName='view_maret'
+          />
+        </Paper>
+      </Grid>
+    </Grid>
+  </Container>
 }
 
 export default StatisticPage;
