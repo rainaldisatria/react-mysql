1322c1bebfacc2bd86a6e5405b5c3b6267d86dc3
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-25 16:46:14 +0700

    Update PersediaanPage.js

--- client/src/container/AdminPage/Produk/Persediaan/PersediaanPage.js
+++ client/src/container/AdminPage/Produk/Persediaan/PersediaanPage.js
@@ -1,8 +1,8 @@
-import { Container, CssBaseline, makeStyles, Typography } from '@material-ui/core';
+import { Container, CssBaseline, makeStyles, Typography, Box } from '@material-ui/core';
 import React from 'react';
 import EditableTable from '../../../../components/EditableTable/EditableTable'
 
-const useStyles = makeStyles(theme => ({  
+const useStyles = makeStyles(theme => ({
     container: {
         paddingTop: theme.spacing(8),
         paddingBottom: theme.spacing(4),
@@ -13,7 +13,7 @@ const PersediaanPage = () => {
     const classes = useStyles();
 
     return (
-        <Container component='main' maxWidth='lg' className={classes.container}>
+        <Container component='main' maxWidth='md' className={classes.container}>
             <CssBaseline />
             <Typography component="h1" variant="h2" align="left" color="textPrimary" gutterBottom>
                 Tabel Persediaan
