3d077de9fe7e358346cff323ebb472651cba3aff
Rainaldi Satria <57592497+rainaldisatria@users.noreply.github.com>
2021-08-10 21:34:42 +0700

    Revert "create module for handling user route"
    This reverts commit a6a7dbd39e83ab668d5b07fa98acc1d777ae576f.

--- server/index.js
+++ server/index.js
@@ -2,19 +2,15 @@ const express = require('express');
 const expressApp = express();
 const mysql = require('mysql');
 const cors = require('cors');
-const db = require('./db') 
-const { getMonthlyIncome } = require('./controllers/tabelTransaksiController');
-const { fetchJumlahPersediaan } = require('./controllers/tabelPersediaanController');
-const { fetchCartData, selectTable, showTables, deleteTable, updateTableName, insertTable, table, descTable, buy, getAnalyticTable } = require('./controllers/adminController');
-const { removeCartItem, setCartItemQuantity, addToCart, fetchCart } = require('./controllers/cartController');
- 
-expressApp.use(express.urlencoded({ extended: true }));
-expressApp.use(express.json());
-expressApp.use(cors());
+const db = require('./db')
 
 db.authenticate().then(() => {
     console.log("connected to mysql")
 })
+
+expressApp.use(express.urlencoded({ extended: true }));
+expressApp.use(express.json());
+expressApp.use(cors());
 
 const currentDB = mysql.createConnection({
     user: 'root',
@@ -50,9 +46,18 @@ expressApp.post('/addToCart', (req, res) => addToCart(req, res, currentDB))
 expressApp.post('/fetchCart', (req, res) => fetchCart(req, res, currentDB))
 //#endregion
  
-//#region users   
-const userRoutes = require('./routes/userRoutes')
-expressApp.use("", userRoutes)
+//#region users  
+const { signup, login, fetchAccount } = require('./controllers/usersController') 
+const { getMonthlyIncome } = require('./controllers/tabelTransaksiController');
+const { fetchJumlahPersediaan } = require('./controllers/tabelPersediaanController');
+const { fetchCartData, selectTable, showTables, deleteTable, updateTableName, insertTable, table, descTable, buy, getAnalyticTable } = require('./controllers/adminController');
+const { removeCartItem, setCartItemQuantity, addToCart, fetchCart } = require('./controllers/cartController');
+
+expressApp.post('/fetchAccount', (req, res) => fetchAccount(req, res, currentDB))
+
+expressApp.post('/signup', signup)
+
+expressApp.post('/login', login)
 //#endregion
 
 //#region Database management 
